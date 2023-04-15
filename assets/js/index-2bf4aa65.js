db2361d42166f8f75d043b080e75463c468f7c65
yuqiang <qiang.yu@paat.com>
2023-04-15 15:10:33 +0800

    dev

--- assets/js/index-2bf4aa65.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./index-36f4f8f5.js";function r(){return e("div",{children:"TestDefect"})}export{r as default};
--- assets/js/index-96237640.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-36f4f8f5.js";const e=()=>r("iframe",{src:"https://cn.bing.com/",frameBorder:"0",className:"card full-iframe"});export{e as default};
